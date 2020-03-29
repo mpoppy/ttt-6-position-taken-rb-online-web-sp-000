b2a553f15ef34d2535caea1b7dc1b36d24b8608b
Marc Poppy <poppy.marc@gmail.com>
2020-03-29 20:57:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -11,3 +11,20 @@ def position_taken?(board, index)
     taken = true
   end
 end
+
+def position_taken?(board, index)
+  # basic solution:
+
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+
+end
